02dfa25394efc4382b2cee8e51a2d3f4093878ff
Eva Graff <stuff@eva.fish>
2024-11-18 18:45:15 -0500

    update build.zig nicer

--- build.zig
+++ build.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // this allows other code that depends on this
+    // one to not have to re-declare the module.
+    const clap_bindings_module = b.addModule(
+        "clap-bindings",
+        .{ .root_source_file = b.path("src/main.zig") },
+    );
+    _ = clap_bindings_module;
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "clap-bindings",
+    const tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
+    const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_tests.step);
+
+    b.default_step.dependOn(test_step);
 }
