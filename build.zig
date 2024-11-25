fb1f4fc4ba7fc29dbaeb4b7f239f6a279462bc9a
Eva Graff <dev@eva.fish>
2024-11-24 23:47:03 -0500

    add documentation build step
    sorta broken still on 0.13.0. i'm putting it in there so when 0.14.0
    comes out, and zig's doc generation hopefully works better, i can just
    write another step in the .build.yml to upload the docs to like
    interpunct.srht.site/clap-zig-bindings/ or something.

--- build.zig
+++ build.zig
@@ -12,14 +12,27 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const test_step = b.step("test", "Run unit tests");
     const tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
     const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
-
     b.default_step.dependOn(test_step);
+
+    const doc_step = b.step("doc", "Generate autodocs");
+    const docs_test = b.addObject(.{
+        .name = "clap-bindings",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = .Debug,
+    });
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = docs_test.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    doc_step.dependOn(&install_docs.step);
 }
